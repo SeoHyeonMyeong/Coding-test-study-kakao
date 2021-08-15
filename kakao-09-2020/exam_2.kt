b56205dbb8d00efb41ff4238303f40cd2d4d8cf6
Seo <gusaud2199@naver.com>
2021-08-15 15:54:27 +0900

    Add: exam_2

---
+++ kakao-09-2020/exam_2.kt
@@ -0,0 +1,12 @@
+/*
+ * 메뉴 리뉴얼
+ * https://programmers.co.kr/learn/courses/30/lessons/72411
+ */
+
+fun main() {
+    var orders = listOf("ABCFG", "AC", "CDE", "ACDE", "BCFG", "ACDEH")
+    var course = listof(2,3,4)
+    println(orders)
+    println(course)
+
+}
