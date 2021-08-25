d8f8d153d74fe7d9730b8f0e2625ffe4f30dd0ad
SeoHyeonMyeong <gusaud2199@naver.com>
2021-08-25 21:02:01 +0900

    Update: 앞뒤에 공백이 오는 경우 생기는 런타임 에러 해결

--- test/JadenCase 문자열 만들기.py
+++ test/JadenCase 문자열 만들기.py
@@ -1,14 +1,17 @@
-s = "3people unFoll3owed me"
+s = " adgagd 3eagdag "
 
 words = s.split(" ")
 print(words)
 
 result = ""
 for word in words:
-    if word[0].isalpha():
+    print(word)
+    if word == '':
+        pass
+    elif word[0].isalpha():
         word = word[0].upper() + word[1:].lower()
     else:
         word = word.lower()
-    result = result + word + " "
+    result += word + " "
 result = result[:-1]
 print(result)
